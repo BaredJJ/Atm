1f1aefbed58192aba04d8e1f10221bd731f7552f
BaredJJ <33960321+BaredJJ@users.noreply.github.com>
2018-11-08 01:57:58 +0300

    Study dinamic add element on page

---
+++ lib/src/ListBoxValue.dart
@@ -0,0 +1,10 @@
+
+class ListBoxValue{
+  int _value;
+
+  int get Value => _value;
+  set Value(int value){
+    if(value != _value)
+      _value = value;
+  }
+}
--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,37 @@
 import 'package:angular/angular.dart';
 import 'package:Atm/app_component.template.dart' as ng;
+import 'dart:html';
 
 void main() {
   runApp(ng.AppComponentNgFactory);
+
+  var values = GetListBoxValues();
+  //var listBox = new SelectElement();
+  SelectElement listBox = querySelector("#first");
+
+  var keys = values.keys.toList();
+  for(int i = 0; i < values.length; i++)
+   {
+      var data = keys[i].toString();
+      var option = new OptionElement();
+      option.text = data;
+      option.value = data;
+      option.selected = false;
+     // var option = new Element.html("<option>" + keys[i].toString()+"</option");
+      listBox.options.add(option);
+      values[keys[i]] = option;
+   }
+
+
+  //container.nodes.add(listBox);
+  //container.children.add(listBox);
+}
+
+Map<int, OptionElement> GetListBoxValues(){
+  var values = new Map<int, OptionElement>();
+
+  for(int i = 2; i < 10; i++)
+    values.putIfAbsent(i, () => null);
+
+  return values;
 }
